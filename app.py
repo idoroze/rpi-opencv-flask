e08ef596b421a2e83e330f0a18274f8086267f76
onedoit <37774781+onedoit@users.noreply.github.com>
2021-05-17 23:50:52 +0300

    change get to post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response,jsonify
+from flask import Flask, render_template, Response,jsonify,request,redirect,url_for
 import cv2
 
 app = Flask(__name__)
@@ -24,14 +24,28 @@ def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-@app.route('/set_stat/<int:i>',)
-def setVal(i):
-    with open('test.txt','w+') as db:
-        db.write(f"{i}")
-        db.close()
-    return jsonify({"data":i})
+@app.route('/set_stat', methods=['GET','POST'])
+def setVal():
+    if request.method=='POST':
+        i = request.form['num']
+        i =int(i)
+        if i>=1 and i<=3:
+            with open('test.txt','w+') as db:
+                db.write(f"{i}")
+                db.close()
+            return jsonify({"data":i})
+        else:
+            return redirect(url_for('/get_stat'))
     
+         
+
+# @app.route('/set_stat/<int:i>',)
+# def setVal(i):
+#   with open('test.txt','w+') as db:
+#       db.write(f"{i}")
+#       db.close()
+#   return jsonify({"data":i})
+     
 @app.route('/get_stat',methods=['GET'])
 def getVal():
     with open('test.txt', 'r') as db:
--- blueToHttp.py
+++ blueToHttp.py
@@ -1,7 +1,7 @@
 import serial
 import requests
 if __name__ == '__main__':
-    url = "http://192.168.1.203:5000/"
+    url = "http://127.0.0.1:5000/"
     ser = serial.Serial('COM4', 9600, timeout=1)
     ser.flush()
     while True:
@@ -9,11 +9,11 @@ if __name__ == '__main__':
             line = ser.readline().decode('utf-8').rstrip()
             try:
                 if line == "ua":
-                    requests.get(url + "set_stat/1")
+                    requests.post(url+'set_stat',{"num":1})
                 if line == "a":
-                    requests.get(url + "set_stat/2")
+                    requests.post(url+'set_stat',{"num":2})
                 if line == "b":
-                    requests.get(url + "set_stat/3")
+                    requests.post(url+'set_stat',{"num":3})
 
             except:
                 print("error appear")
