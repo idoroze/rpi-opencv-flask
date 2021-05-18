1ab080e4834f6404385b4d91720097f5ad263ea6
Ubuntu <ubuntu@ubuntu.lan>
2021-05-18 14:24:52 +0000

    dynmic ip

--- SerialToHttp.py
+++ SerialToHttp.py
@@ -1,8 +1,11 @@
 import serial
 import requests
+import socket
 if __name__ == '__main__':
-    url = "http://127.0.0.1:5000/"
-    ser = serial.Serial('COM4', 9600, timeout=1)
+    hostname = socket.gethostname()
+    ip = socket.gethostbtname(hostname)
+    url = "http://"+str(ip)":5000/"
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     while True:
         if ser.in_waiting > 0:
